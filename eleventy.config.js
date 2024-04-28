b7ce3ba39e6578bf97c7f90afbf390f7b1236f51
Roy Mosby <ego.madking@gmail.com>
2024-04-27 23:23:45 -0400

    add passthroughs

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,10 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/assets');
   eleventyConfig.addPassthroughCopy('src/styles');
   eleventyConfig.addPassthroughCopy('src/scripts');
+  eleventyConfig.addPassthroughCopy('src/site.webmanifest');
+  eleventyConfig.addPassthroughCopy('src/icon.png');
+  eleventyConfig.addPassthroughCopy('src /icon.svg');
+  eleventyConfig.addPassthroughCopy('src/favicon.ico');
 
   return {
     dir: {
