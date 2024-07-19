8a0144d31959481bdcf421d3a01f5c452f0f63dc
Hayley Witherell <hayleywitherell@gmail.com>
2024-07-19 13:32:49 -0600

    Make themes affect colors & hide media tab

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,15 +1,66 @@
-// theme toggle
-
 $(document).ready(function () {
+  loadSavedTheme();
   updateThemeColors();
 
-  $('.theme-icon').on('click', function () {
-    $('.sun-icon, .moon-icon').toggleClass('hidden');
-    updateThemeColors();
+  $(".theme-icon").on("click", function () {
+    toggleTheme();
   });
 
+  function applyTheme(theme) {
+    const elements = [
+      "body",
+      ".title",
+      "nav",
+      ".nav-item",
+      "footer"
+    ];
+
+    elements.forEach(selector => {
+      const $el = $(selector);
+      if (theme === "dark") {
+        $el.addClass("dark-mode").removeClass("light-mode");
+      } else {
+        $el.addClass("light-mode").removeClass("dark-mode");
+      }
+    });
+  }
+
+  function loadSavedTheme() {
+    const savedTheme = localStorage.getItem("theme") || "light";
+    if (savedTheme === "dark") {
+      $(".sun-icon").addClass("hidden");
+      $(".moon-icon").removeClass("hidden");
+    } else {
+      $(".moon-icon").addClass("hidden");
+      $(".sun-icon").removeClass("hidden");
+    }
+    applyTheme(savedTheme);
+  }
+
+  function saveThemePreference() {
+    const newTheme = $(".sun-icon").is(":visible") ? "dark" : "light";
+    localStorage.setItem("theme", newTheme);
+  }
+
+  function toggleTheme() {
+    $(".sun-icon, .moon-icon").toggleClass("hidden");
+    saveThemePreference();
+    updateThemeColors();
+  }
+
   function updateThemeColors() {
-    const [color1, color2] = $('.sun-icon').is(':visible') ? ['white', 'black'] : ['black', 'white'];
-    $(':root').css({ '--black': color1, '--white': color2 });
+    let color1, color2;
+    const theme = $(".sun-icon").is(":visible") ? "dark" : "light";
+
+    if (theme === "dark") {
+      color1 = "white";
+      color2 = "black";
+    } else {
+      color1 = "black";
+      color2 = "white";
+    }
+
+    $(":root").css({ "--black": color1, "--white": color2 });
+    applyTheme(theme);
   }
 });
