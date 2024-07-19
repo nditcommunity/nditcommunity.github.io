3336d650824eaf6fa54c1d5d45a486a0dd140605
Hayley Witherell <hayleywitherell@gmail.com>
2024-07-19 16:21:38 -0600

    Run prettier

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,36 +1,29 @@
 // theme management
 
 $(document).ready(() => {
-
   const applyTheme = (themeClass) => {
-
     // update theme class
-    $("body").removeClass("night-mode day-mode").addClass(themeClass);
+    $('body').removeClass('night-mode day-mode').addClass(themeClass);
 
     // update theme icons
-    const isDarkMode = themeClass === "night-mode";
-    $(".sun-icon").toggleClass("hidden", !isDarkMode);
-    $(".moon-icon").toggleClass("hidden", isDarkMode);
+    const isDarkMode = themeClass === 'night-mode';
+    $('.sun-icon').toggleClass('hidden', !isDarkMode);
+    $('.moon-icon').toggleClass('hidden', isDarkMode);
   };
 
   // load previously saved theme or set default to night-mode
-  const savedTheme = localStorage.getItem("theme") || "night-mode";
+  const savedTheme = localStorage.getItem('theme') || 'night-mode';
   applyTheme(savedTheme);
 
   // toggle new theme
-  $(".theme-icon").on("click", () => {
-
+  $('.theme-icon').on('click', () => {
     // determine which theme to apply
-    const currentTheme = $("body").hasClass("night-mode")
-      ? "night-mode"
-      : "day-mode";
+    const currentTheme = $('body').hasClass('night-mode') ? 'night-mode' : 'day-mode';
 
-    const newTheme = currentTheme === "night-mode"
-      ? "day-mode"
-      : "night-mode";
+    const newTheme = currentTheme === 'night-mode' ? 'day-mode' : 'night-mode';
 
     // save and apply new theme
-    localStorage.setItem("theme", newTheme);
+    localStorage.setItem('theme', newTheme);
     applyTheme(newTheme);
   });
 });
