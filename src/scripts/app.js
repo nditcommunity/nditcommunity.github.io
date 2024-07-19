9500afe944a6c4711c193d1c8b2eb8d50e3a2383
Hayley Witherell <hayleywitherell@gmail.com>
2024-07-19 15:19:37 -0600

    Change light/dark to day/night

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,29 +5,29 @@ $(document).ready(() => {
   const applyTheme = (themeClass) => {
 
     // update theme class
-    $("body").removeClass("dark-mode light-mode").addClass(themeClass);
+    $("body").removeClass("night-mode day-mode").addClass(themeClass);
 
     // update theme icons
-    const isDarkMode = themeClass === "dark-mode";
+    const isDarkMode = themeClass === "night-mode";
     $(".sun-icon").toggleClass("hidden", !isDarkMode);
     $(".moon-icon").toggleClass("hidden", isDarkMode);
   };
 
-  // load previously saved theme
-  const savedTheme = localStorage.getItem("theme") || "light-mode";
+  // load previously saved theme or set default to night-mode
+  const savedTheme = localStorage.getItem("theme") || "night-mode";
   applyTheme(savedTheme);
 
   // toggle new theme
   $(".theme-icon").on("click", () => {
 
     // determine which theme to apply
-    const currentTheme = $("body").hasClass("dark-mode")
-      ? "dark-mode"
-      : "light-mode";
+    const currentTheme = $("body").hasClass("night-mode")
+      ? "night-mode"
+      : "day-mode";
 
-    const newTheme = currentTheme === "dark-mode"
-      ? "light-mode"
-      : "dark-mode";
+    const newTheme = currentTheme === "night-mode"
+      ? "day-mode"
+      : "night-mode";
 
     // save and apply new theme
     localStorage.setItem("theme", newTheme);
