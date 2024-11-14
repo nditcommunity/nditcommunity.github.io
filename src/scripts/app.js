f038c0a7b5d67fdc2259d6595d12cc913b1901e7
Hayley Witherell <hayleywitherell@gmail.com>
2024-11-14 11:52:16 -0700

    Run prettier

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,33 +1,33 @@
 $(document).ready(() => {
   const applyTheme = (themeClass) => {
     // update theme class
-    $("body").removeClass("night-mode day-mode").addClass(themeClass);
+    $('body').removeClass('night-mode day-mode').addClass(themeClass);
 
     // update theme icons
-    const isDarkMode = themeClass === "night-mode";
-    $(".sun-icon").toggleClass("hidden", !isDarkMode);
-    $(".moon-icon").toggleClass("hidden", isDarkMode);
+    const isDarkMode = themeClass === 'night-mode';
+    $('.sun-icon').toggleClass('hidden', !isDarkMode);
+    $('.moon-icon').toggleClass('hidden', isDarkMode);
 
     // update all social icons dynamically
-    $(".social-icon").each(function() {
-      const src = $(this).attr("src");
+    $('.social-icon').each(function () {
+      const src = $(this).attr('src');
       const newSrc = src.replace(/-(black|white)\.png$/, `-${isDarkMode ? 'white' : 'black'}.png`);
-      $(this).attr("src", newSrc);
+      $(this).attr('src', newSrc);
     });
   };
 
   // load previously saved theme or set default to night-mode
-  const savedTheme = localStorage.getItem("theme") || "night-mode";
+  const savedTheme = localStorage.getItem('theme') || 'night-mode';
   applyTheme(savedTheme);
 
   // toggle new theme
-  $(".theme-icon").on("click", () => {
+  $('.theme-icon').on('click', () => {
     // determine which theme to apply
-    const currentTheme = $("body").hasClass("night-mode") ? "night-mode" : "day-mode";
-    const newTheme = currentTheme === "night-mode" ? "day-mode" : "night-mode";
+    const currentTheme = $('body').hasClass('night-mode') ? 'night-mode' : 'day-mode';
+    const newTheme = currentTheme === 'night-mode' ? 'day-mode' : 'night-mode';
 
     // save and apply new theme
-    localStorage.setItem("theme", newTheme);
+    localStorage.setItem('theme', newTheme);
     applyTheme(newTheme);
   });
 });
